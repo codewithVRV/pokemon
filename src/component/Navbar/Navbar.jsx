2fbbdd9e48b4d9860e61483779c5bd38a550381c
Vishnu Kumar <vermaji1891@gmail.com>
2023-10-10 13:49:06 +0530

    added particular details of pokemon

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import usePokemonData from '../hooks/usePokemonData';
 import './Navbar.css'
 import { useState } from 'react';
@@ -6,6 +7,11 @@ function Navbar () {
     const [isAutoCompleteVisible, setIsAutoCompleteVisible] = useState(false)
     const [searchTerm, setSearchTerm] = useState("")
     const [singleData] = usePokemonData()
+    const navigator = useNavigate()
+    function handleInput (id) {
+        console.log("Id is", id)
+        navigator(`poke/${id}`)
+    }
     // console.log("searchTerm", searchTerm)
     return (
         <>  
@@ -17,10 +23,8 @@ function Navbar () {
                         onChange={(e) => setSearchTerm(e.target.value)}
                     />
                     <div className="auto-result" style={{display: (isAutoCompleteVisible) ? 'block': 'none'}}>
-                        {singleData.length > 0 && singleData.map((data) => <p key={data.data.id} >{data.data.name}</p>)}
-                        {/* <p>Vishnu</p>
-                        <p>Vishnu</p>
-                        <p>Vishnu</p> */}
+                        {singleData.length > 0 && singleData.map((data) => <p key={data.data.id} onMouseDown={() => handleInput(data.data.id)} >{data.data.name}</p>)}
+                        
                     </div>
                 </div>
                 
--- src/pages/PokeDetails/PokeDetails.jsx
+++ src/pages/PokeDetails/PokeDetails.jsx
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './PokeDetails.css'
+
+
 function PokeDetails () {
+    const {id} = useParams()
+    const [card, setCard] = useState(null)
+
+    async function downloadSingleData () {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        console.log('response of movie details data', response.data)
+        setCard(response.data)
+    }
+    console.log("card", card)
+    console.log(typeof card)
+
+    useEffect(() => {
+        downloadSingleData()
+    }, [])
+    
     return (
-        <h1>PokeDetails page</h1>
+        <>
+            { card && <div className="pokemon-wrapper">
+                <div className="p-img">
+                <img src={card.sprites.front_default} alt="" />
+                </div>
+                <div className="p-details">
+                    <p><b>Name:-</b>   {card.name}</p>
+                    <p><b>Ability:-</b>   {card.abilities[0].ability.name}{"  - "}{card.abilities[1].ability.name}</p>
+                    <p><b>Height:-</b> {card.height} cm.</p>
+                    <p><b>Weight:-</b> {card.weight} gm.</p>
+                </div>
+            </div>}
+        </>
     )
 }
 
